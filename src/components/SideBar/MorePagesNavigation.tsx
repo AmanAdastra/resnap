4e6460cc7864bc8f8dbf69df96f6e0d67b662178
Aman Khan <amanayankhan78@gmail.com>
2024-03-05 16:40:45 +0530

    More Pages Navigation

--- src/components/SideBar/MorePagesNavigation.tsx
+++ src/components/SideBar/MorePagesNavigation.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Inbox, Home, MapPin, Zap, Youtube } from 'react-feather'
+import { NavigationTitle } from './NavigationTitle'
+
 
 export const MorePagesNavigation = () => {
-  return (
-    <div>MorePagesNavigation</div>
-  )
+  const sections = [
+    { icon: Inbox, text: 'Email Box' },
+    { icon: Home, text: 'Near Hotel' },
+    { icon: MapPin, text: 'Latest Event' },
+    { icon: Youtube, text: 'Live Stream' },
+];
+
+return (
+    <div className='my-2 pt-4 pb-2  '>
+        <NavigationTitle heading='More Pages' />
+        {
+            sections.map((el, index) => {
+                return (
+                    <>
+                        <div key={index} className='flex h-[54px]  text-[15px] my-[1px] py-[12px] px-[15px] items-center text-gray-400'>
+                            <div className={'h-[45px] w-[45px] rounded-full flex-center '}>
+                                {React.createElement(el.icon, { color: '#05f', size: 28 })}
+                            </div>
+                            <span className='font-medium ml-4 hover:text-blue-500 hover:cursor-pointer'>{el.text}</span>
+                        </div>
+                    </>
+                )
+            })
+        }
+
+    </div>
+)
 }
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NewsFeedNavigation } from './NewsFeedNavigation'
+import { MorePagesNavigation } from './MorePagesNavigation'
 
 export const SideBar = () => {
     return (
         <div className='w-[280px] px-[15px]'>
             <NewsFeedNavigation/>
+            <MorePagesNavigation/>
         </div>
     )
 }
