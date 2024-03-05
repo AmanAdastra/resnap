239aa59248fcfb43b41cdd27ca4a1129b04bc9bc
Aman Khan <amanayankhan78@gmail.com>
2024-03-05 17:41:49 +0530

    scrolling sidebar with fixed topheader

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -5,7 +5,7 @@ import { AccountNavigation } from './AccountNavigation'
 
 export const SideBar = () => {
     return (
-        <div className='w-[280px] px-[15px]'>
+        <div className='w-[280px] h-[100vh] pt-[96px] overflow-y-auto  px-[15px]'>
             <NewsFeedNavigation/>
             <MorePagesNavigation/>
             <AccountNavigation/>
